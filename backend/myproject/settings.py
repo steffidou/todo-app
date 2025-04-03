ae81ffee8e54b95d43766a2b1f29bc97022d1c6f
Udasco, Dou Issa Steffi <160296940+steffidou@users.noreply.github.com>
2025-04-03 23:57:21 +0800

    status updated

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -29,11 +29,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['todo-app-u6pm.onrender.com', 'steffidou.github.io/todo-app/']
 
-CORS_ALLOW_ALL_ORIGINS = True
-
 CORS_ALLOWED_ORIGINS = ['http://steffidou.github.io', 
                         'http://todo-app-u6pm.onrender.com'
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
