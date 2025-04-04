886b051b049d5ffe17bff508b8fbf31d378e8789
Udasco, Dou Issa Steffi <160296940+steffidou@users.noreply.github.com>
2025-04-04 08:03:56 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  server: {
-      proxy: {
-          "/api": {
-              target: "hhttps://todo-app-u6pm.onrender.com/",
-              changeOrigin: true,
-              secure: false,
-          },
-      },
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
   },
 });
